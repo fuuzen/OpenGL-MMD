0bd44c4403265132a65b73a08bf28a24f1119a25
CR Ye <3342711246@qq.com>
2023-11-25 22:15:34 +0800

    add assimp loader

--- PMXLoader/main.cpp
+++ PMXLoader/main.cpp
@@ -1,41 +1,30 @@
-﻿#include <glad/glad.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
 #include <string>
 #include <windows.h>
 
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
-//#define STB_IMAGE_IMPLEMENTATION
-//#include <stb_image.h>
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
+
+#include <common/assimploader.hpp>
 
 int main()
 {
-	std::string path = "model/hutao/hutao.pmx";
-	std::wstring wpath(path.begin(), path.end());
-	OutputDebugStringW((L"加载模型：" + wpath + L"\n").c_str());
-	Assimp::Importer importer;
-	const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
-	if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
-	{
-		OutputDebugStringA(importer.GetErrorString());
-		return 0;
-	}
-	OutputDebugString(L"加载模型成功\n");
-	// mRootNode：根节点，mMetaData：元数据
-	// mMeshes：网格，mMaterials：材质
-	int vericesNum = 0, facesNum = 0;
-	for (unsigned int i = 0; i < scene->mNumMeshes; i++)
-	{
-		OutputDebugStringW((L"网格" + std::to_wstring(i) + L"顶点数：" + std::to_wstring(scene->mMeshes[i]->mNumVertices)).c_str());
-		OutputDebugStringW((L" 面数：" + std::to_wstring(scene->mMeshes[i]->mNumFaces) + L"\n").c_str());
-		vericesNum += scene->mMeshes[i]->mNumVertices;
-		facesNum += scene->mMeshes[i]->mNumFaces;
-	}
-	OutputDebugString(std::to_wstring(vericesNum).c_str());
-	OutputDebugString(L" ");
-	OutputDebugString(std::to_wstring(facesNum).c_str());
-	OutputDebugString(L"\n");
+	std::vector<unsigned short> indices;
+	std::vector<glm::vec3> vertices;
+	std::vector<glm::vec2> uvs;
+	std::vector<glm::vec3> normals;
+	bool res = loadAssImp("model/hutao/hutao.pmx", indices, vertices, uvs, normals);
+
+	OutputDebugStringW((L"顶点数：" + std::to_wstring(vertices.size()) + L"\n").c_str());
+	OutputDebugStringW((L"面数：" + std::to_wstring(indices.size() / 3) + L"\n").c_str());
+
 	return 0;
 }
