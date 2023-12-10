4d8c5be3dfccb9ab88ee22cae68f17a42dc4ce6e
CR Ye <3342711246@qq.com>
2023-12-10 21:46:39 +0800

    wip: change import

--- BoneModel/main.cpp
+++ BoneModel/main.cpp
@@ -1,31 +1,8 @@
-#include <GL/gl3w.h>
-#include <GLFW/glfw3.h>
-#if _WIN32
-#define  GLFW_EXPOSE_NATIVE_WIN32
-#include <GLFW/glfw3native.h>
-#endif // _WIN32
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <string>
 
-#include <iostream>
-#include <fstream>
-#include <memory>
-#include <map>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-
-#define	STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_STATIC
-#include <stb/stb_image.h>
-
-#include <base/Path.h>
-#include <base/File.h>
-#include <base/UnicodeUtil.h>
-#include <base/Time.h>
-#include <base/PMDModel.h>
-#include <base/PMXModel.h>
-#include <base/VMDFile.h>
-#include <base/VMDAnimation.h>
-#include <base/VMDCameraAnimation.h>
 int main(void)
 {
 	return 0;
