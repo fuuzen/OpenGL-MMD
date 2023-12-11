bd2c9a4dd2ea2ef9e60f9ddd3c9b84f1a3c0ccd8
CR Ye <3342711246@qq.com>
2023-12-11 14:14:18 +0800

    wip: add example

---
+++ saba/example/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Ye/source/repos/saba/example
+# Build directory: C:/Users/Ye/source/repos/saba/example
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
